11ea4f44495d2ba3a7e68f1d0172a1b9349f656a
MelanX <info@melanx.de>
2022-12-13 20:07:46 +0100

    add No AI spawn egg
    closes #1

--- src/main/java/de/melanx/datatrader/ModItems.java
+++ src/main/java/de/melanx/datatrader/ModItems.java
@@ -1,5 +1,9 @@
 package de.melanx.datatrader;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
@@ -8,17 +12,40 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.ForgeSpawnEggItem;
 import org.moddingx.libx.annotation.registration.RegisterClass;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
 import java.util.Optional;
 
 @RegisterClass(registry = "ITEMS")
 public class ModItems {
 
     public static final Item traderSpawnEgg = new ForgeSpawnEggItem(() -> ModEntities.dataTrader, 0xEF6231, 0xBD8B72, new Item.Properties().tab(CreativeModeTab.TAB_MISC)) {
+
+        @Override
+        public void fillItemCategory(@Nonnull CreativeModeTab tab, @Nonnull NonNullList<ItemStack> items) {
+            if (this.allowedIn(tab)) {
+                ItemStack egg = new ItemStack(this);
+                ItemStack noAi = egg.copy();
+                CompoundTag entityTag = noAi.getOrCreateTagElement("EntityTag");
+                entityTag.putBoolean("NoAI", true);
+                items.add(egg);
+                items.add(noAi);
+            }
+        }
+
+        @Override
+        public void appendHoverText(@Nonnull ItemStack stack, @Nullable Level level, @Nonnull List<Component> components, @Nonnull TooltipFlag flag) {
+            if (stack.getOrCreateTag().getCompound("EntityTag").getBoolean("NoAI")) {
+                components.add(Component.literal("No AI").withStyle(ChatFormatting.RED));
+            }
+        }
 
         @Nonnull
         @Override
